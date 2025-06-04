f8b36b40361802a104fc1a54a39565caa75cab41
Alanportodeveloper <97985674+Alanportodeveloper@users.noreply.github.com>
2025-06-04 22:58:13 +0000

    Foi implementado o movimento do cavalo utilizando laços de repetição aninhado com while e for

--- xadrez.c
+++ xadrez.c
@@ -7,6 +7,9 @@ int main() {
     int movimentoTorre = 5; // Define que a Torre vai se mover 5 casas para a direita
     int movimentoBispo = 5; // Define que o Bispo vai se mover 5 casas na diagonal
     int movimentoRainha = 8; // Define que a Rainha vai se mover 8 casas para a esquerda
+    int casasBaixo = 2; // Define a quantidade de casas que o Cavalo deve se mover para baixo
+    int casasEsquerda = 1; // Define a quantidade de casas que o Cavalo deve se mover para a esquerda
+
 
 
     printf("Torre se movendo... \n\n");
@@ -37,6 +40,22 @@ int main() {
         printf("Esquerda!\n\n"); // Imprime a direção do movimento da Rainha
         j++; // Incrementa o contador
     } while (j < movimentoRainha); // Continua enquanto j for menor que 8
+
+
+    printf("\n\n****************************************\n\n");
+    printf("Cavalo se movendo... \n\n");
+    int k = 0; 
+
+    while (k < 1) { // Executa o movimento do cavalo apenas uma vez
+
+        // Loop for aninhado para as duas casas para baixo
+        for (int l = 0; l < casasBaixo; l++) {
+            printf("Baixo!\n");
+        }
+        // Movimento perpendicular (uma casa para a esquerda)
+        printf("Esquerda!\n\n");
+        k++; // Após executar o movimento do Cavalo, incrementa k para que o loop while não se repita mais (executa apenas uma vez)
+    }
 
     return 0;
 }
